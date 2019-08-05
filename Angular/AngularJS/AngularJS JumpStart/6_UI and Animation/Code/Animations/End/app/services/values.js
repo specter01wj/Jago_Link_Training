08d529cd5691d346a69dc636cc3be836812de213
specter01wj <specter01wj@gmail.com>
2019-08-04 23:56:03 -0400

    07/25/2019 v1

--- Angular/AngularJS/AngularJS JumpStart/6_UI and Animation/Code/Animations/End/app/services/values.js
+++
@@ -1,9 +0,0 @@
-angular.module('customersApp').value('appSettings', {
-    title: 'Customers Application',
-    version: '1.0'
-});
-
-/*angular.module('customersApp').constant('appSettings', {
-    title: 'Customers Application',
-    version: '1.0'
-});*/
