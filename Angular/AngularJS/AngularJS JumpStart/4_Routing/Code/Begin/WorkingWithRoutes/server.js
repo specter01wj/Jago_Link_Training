e676293f93e82355973d779e22c3f49bd4986020
specter01wj <specter01wj@gmail.com>
2019-08-04 20:31:42 -0400

    07/25/2019 v1

--- Angular/AngularJS/AngularJS JumpStart/4_Routing/Code/Begin/WorkingWithRoutes/server.js
+++ Angular/AngularJS/AngularJS JumpStart/4_Routing/Code/Begin/WorkingWithRoutes/server.js
@@ -1,15 +1,61 @@
-(function() {
-    
-    var CustomersController = function ($scope) {
-        $scope.sortBy = 'name';
-        $scope.reverse = false;
-        
-        $scope.doSort = function(propName) {
-           $scope.sortBy = propName;
-           $scope.reverse = !$scope.reverse;
-        };
-        
-        $scope.customers= [
+var express = require('express'),
+    app = express();
+
+//Express 3
+//app.configure(function() {
+//    app.use(express.static(__dirname, '/'));
+//});
+
+//Express 4
+app.use(express.static(__dirname + '/'));
+
+app.get('/customers/:id', function(req, res) {
+    var customerId = parseInt(req.params.id);
+    var data = {};
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].id === customerId) {
+           data = customers[i];
+           break;
+        }
+    }  
+    res.json(data);
+});
+
+app.get('/customers', function(req, res) {
+    res.json(customers);
+    //res.json(500, { error: 'An error has occurred!' });
+});
+
+app.get('/orders', function(req, res) {
+    var orders = [];
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].orders) {
+            for (var j=0,ordersLen=customers[i].orders.length;j<ordersLen;j++) {
+                orders.push(customers[i].orders[j]);   
+            }
+        }
+    }  
+    res.json(orders);
+});
+
+app.delete('/customers/:id', function(req, res) {
+    var customerId = parseInt(req.params.id);
+    var data = { status: true };
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].id === customerId) {
+           customers.splice(i,1);
+           data = { status: true };
+           break;
+        }
+    }  
+    res.json(data);
+});
+
+app.listen(8080);
+
+console.log('Express listening on port 8080');
+
+        var customers = [
             {
                 id: 1, 
                 joined: '2000-12-02', 
@@ -72,11 +118,3 @@
                 ]
             }
         ];
-    };
-    
-    CustomersController.$inject = ['$scope'];
-
-    angular.module('customersApp')
-      .controller('CustomersController', CustomersController);
-    
-}());
--- Angular/AngularJS/AngularJS JumpStart/4_Routing/Code/End/WorkingWithRoutes/server.js
+++ Angular/AngularJS/AngularJS JumpStart/4_Routing/Code/End/WorkingWithRoutes/server.js
@@ -1,20 +1,61 @@
-(function() {
-    
-    var OrdersController = function ($scope, $routeParams) {
-        var customerId = $routeParams.customerId;
-        $scope.orders = null;
-        
-        function init() {
-            //Search the customers for the customerId
-            for (var i=0,len=$scope.customers.length;i<len;i++) {
-               if ($scope.customers[i].id === parseInt(customerId)) {
-                   $scope.orders = $scope.customers[i].orders;
-                   break;
-               }
+var express = require('express'),
+    app = express();
+
+//Express 3
+//app.configure(function() {
+//    app.use(express.static(__dirname, '/'));
+//});
+
+//Express 4
+app.use(express.static(__dirname + '/'));
+
+app.get('/customers/:id', function(req, res) {
+    var customerId = parseInt(req.params.id);
+    var data = {};
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].id === customerId) {
+           data = customers[i];
+           break;
+        }
+    }  
+    res.json(data);
+});
+
+app.get('/customers', function(req, res) {
+    res.json(customers);
+    //res.json(500, { error: 'An error has occurred!' });
+});
+
+app.get('/orders', function(req, res) {
+    var orders = [];
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].orders) {
+            for (var j=0,ordersLen=customers[i].orders.length;j<ordersLen;j++) {
+                orders.push(customers[i].orders[j]);   
             }
         }
-        
-        $scope.customers= [
+    }  
+    res.json(orders);
+});
+
+app.delete('/customers/:id', function(req, res) {
+    var customerId = parseInt(req.params.id);
+    var data = { status: true };
+    for (var i=0,len=customers.length;i<len;i++) {
+        if (customers[i].id === customerId) {
+           customers.splice(i,1);
+           data = { status: true };
+           break;
+        }
+    }  
+    res.json(data);
+});
+
+app.listen(8080);
+
+console.log('Express listening on port 8080');
+
+        var customers = [
             {
                 id: 1, 
                 joined: '2000-12-02', 
@@ -77,13 +118,3 @@
                 ]
             }
         ];
-        
-        init();
-    };
-    
-    OrdersController.$inject = ['$scope', '$routeParams'];
-
-    angular.module('customersApp')
-      .controller('OrdersController', OrdersController);
-    
-}());
