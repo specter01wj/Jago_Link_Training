3cc4a9e7708de0cdb279952944b489a9f43fc787
specter01wj <specter01wj@gmail.com>
2019-09-25 18:25:16 -0400

    09/05/2019 v1

--- Angular/Angular7/TypeScript Fundamentals/2.Typing, Variables, and Functions/Code/Before/TypeScriptTypes/Scripts/02-02-timer.ts
+++ Angular/Angular7/TypeScript Fundamentals/2.Typing, Variables, and Functions/Code/Before/TypeScriptTypes/Scripts/02-02-timer.ts
@@ -1,66 +1,71 @@
-var
-    // Basics properties
-    timerId = -1,
-    interval = 25,
-    ms = 0,
-    seconds = 0,
-    minutes = 0,
+// namespace is the preferred keyword over module
+namespace demo_02_02 {
+    var
+        // Basics properties
+        timerId: number = -1,
+        interval: number = 25,
+        ms: number = 0,
+        seconds: number = 0,
+        minutes: number = 0,
 
-    // Kick the timer off with an interval
-    startTimer = function () {
-        if (timerId === -1) {
-            timerId = window.setInterval('turnTimerOn()', interval);
-        }
-    },
+        // Kick the timer off with an interval
+        startTimer = function () {
+            if (timerId === -1) {
+                timerId = window.setInterval('demo_02_02.turnTimerOn()', interval);
+            }
+        },
 
-    // Update the screen with the timer data
-    displayTimer = function () {
-        document.getElementById('milliseconds').innerHTML = ms.toString();
-        document.getElementById('seconds').innerHTML = second.toString();
-        document.getElementById('minutes').innerHTML = minutes.toString();
-    },
+        // Update the screen with the timer data
+        displayTimer = function () {
+            document.getElementById('milliseconds').innerHTML = ms.toString();
+            document.getElementById('seconds').innerHTML = seconds.toString();
+            document.getElementById('minutes').innerHTML = minutes.toString();
+        },
 
-    // Start counting
-    turnTimerOn = function () {
-        ms += interval;
-        if (ms >= 1000) {
-            ms = 0;
-            seconds += 1;
-        }
-        if (seconds >= 60) {
+        // Pause the timer
+        pauseTimer = function () {
+            window.clearInterval(timerId);
+            timerId = -1;
+        },
+
+        // Reset the timer to 0
+        clearTimer = function () {
+            pauseTimer();
             ms = 0;
             seconds = 0;
-            minutes += 1;
-        }
-        displayTimer(true);
-    },
+            minutes = 0;
+            displayTimer();
+        },
 
-    // Pause the timer
-    pauseTimer = function () {
-        window.clearInterval(timerId);
-        timerId = -1;
-    },
+        // Bootstrap everything
+        init: (s: string, p: string, c: string) => void
+            = function (startButton, pauseButton, clearButton) {
+                document.getElementById(startButton).
+                    addEventListener("click", startTimer, false);
+                document.getElementById(pauseButton).
+                    addEventListener("click", pauseTimer, false);
+                document.getElementById(clearButton).
+                    addEventListener("click", clearTimer, false);
+                displayTimer();
+            };
 
-    // Reset the timer to 0
-    clearTimer = function () {
-        pauseTimer();
-        ms = 0;
-        seconds = 0;
-        minutes = 0;
-        displayTimer();
-    },
+            // Start counting
+        export function turnTimerOn () {
+            ms += interval;
+            if (ms >= 1000) {
+                ms = 0;
+                seconds += 1;
+            }
+            if (seconds >= 60) {
+                ms = 0;
+                seconds = 0;
+                minutes += 1;
+            }
+            displayTimer();
+        };
 
-    // Bootstrap everything
-    init = function (startButton, pauseButton, clearButton) {
-        document.getElementById(startButton).
-            addEventListener("click", startTimer, false);
-        document.getElementById(pauseButton).
-            addEventListener("click", pauseTimer, false);
-        document.getElementById(clearButton).
-            addEventListener("click", clearTimer, false);
-        displayTimer();
+
+    window.onload = function () {
+        init('startButton', 'pauseButton', 'clearButton');
     };
-
-window.onload = function () {
-    init('startButton', 'pauseButton');
-};
+}
