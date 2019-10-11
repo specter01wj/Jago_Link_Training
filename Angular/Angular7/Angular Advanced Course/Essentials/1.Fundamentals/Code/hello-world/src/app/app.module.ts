50aea93c812bebf7d85ad73df33c1b46d4618e04
specter01wj <specter01wj@gmail.com>
2019-10-11 02:26:20 -0400

    09/05/2019 v1

---
+++ Angular/Angular7/Angular Advanced Course/Essentials/1.Fundamentals/Code/hello-world/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CoursesService } from './service/courses.service';
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
