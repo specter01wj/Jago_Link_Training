292b50aaca5feca1de4c88f63afa5579c215b27a
specter01wj <specter01wj@gmail.com>
2019-12-30 13:14:44 -0500

    12/27/2019 v1

--- Angular/AngularJS/AngularJS JumpStart/5_Factories and Services/Code/UsingFactoriesAndServices/mysol_3_values/app/services/values.js
+++ Angular/AngularJS/AngularJS JumpStart/5_Factories and Services/Code/UsingFactoriesAndServices/mysol_3_values/app/services/values.js
@@ -2,12 +2,12 @@
 
 /*angular.module('customersApp')
       .value('appSettings', {
-          title: "Lord of Rings",
+          title: "Lord of Rings_Value",
           version: "1.1"
       });*/
 
 angular.module('customersApp')
       .constant('appSettings', {
-          title: "Lord of Rings",
+          title: "Lord of Rings_Constant",
           version: "1.1"
       });
