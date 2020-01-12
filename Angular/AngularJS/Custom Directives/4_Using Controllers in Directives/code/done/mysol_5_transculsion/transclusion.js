84ef6879a65fc1d017f52a99218d875afbea07a3
specter01wj <specter01wj@gmail.com>
2020-01-11 21:12:11 -0500

    01/06/2020 v1

--- Angular/AngularJS/Custom Directives/4_Using Controllers in Directives/code/done/mysol_5_transculsion/transclusion.js
+++ Angular/AngularJS/Custom Directives/4_Using Controllers in Directives/code/done/mysol_5_transculsion/transclusion.js
@@ -12,9 +12,9 @@
 
           vm.addTask = function () {
 
-              if (!vm.tasks) vm.tasks = [];
+              if (!vm.tasks) {vm.tasks = [];
 
-              
+
 
               vm.tasks.push({
                 title: vm.title
