6fa612279cbbcb53cc3ae5a59db07abcea1f7fe5
specter01wj <specter01wj@gmail.com>
2020-01-16 12:18:00 -0500

    01/11/2020 v1

--- Angular/AngularJS/Deep dive into scope/code/done/step_4/main.js
+++ Angular/AngularJS/Deep dive into scope/code/done/step_4/main.js
@@ -1,17 +1,17 @@
 var app = angular.module('codecraft', []);
 
 app.controller('ParentController', function ($scope, $rootScope) {
-	
+	/*$scope.name = "Parent";
 	$scope.reset = function () {
 		$scope.name = "Parent";
-	};
+	};*/
 
 });
 
 app.controller('ChildController', function ($scope, $rootScope) {
 	
 	$scope.reset = function () {
-		$rootScope.name = "Reset By Child";
+		$rootScope.data.name = "Reset By Child";
 	};
 
 });
