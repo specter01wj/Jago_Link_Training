98afaaf625815bff5f2435b3b60ffc056c823844
specter01wj <specter01wj@gmail.com>
2020-02-07 19:14:45 -0500

    02/07/2020 v1

---
+++ Angular/Angular7/TypeScript Fundamentals/3.Classes and Interfaces/Code/Before/done/TypeExtension/TypeExtension/Scripts/auto.js
@@ -0,0 +1,107 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Engine = /** @class */ (function () {
+    function Engine(horsePower, engineType) {
+        this.horsePower = horsePower;
+        this.engineType = engineType;
+    }
+    Engine.prototype.start = function (callback) {
+        var _this = this;
+        window.setTimeout(function () {
+            callback(true, _this.engineType);
+        }, 1000);
+    };
+    Engine.prototype.stop = function (callback) {
+        var _this = this;
+        window.setTimeout(function () {
+            callback(true, _this.engineType);
+        }, 1000);
+    };
+    return Engine;
+}());
+var Accessory = /** @class */ (function () {
+    function Accessory(accessoryNumber, title) {
+        this.accessoryNumber = accessoryNumber;
+        this.title = title;
+    }
+    return Accessory;
+}());
+var Auto = /** @class */ (function () {
+    function Auto(basePrice, engine, make, model) {
+        this.engine = engine;
+        this.basePrice = basePrice;
+        this.make = make;
+        this.model = model;
+    }
+    Object.defineProperty(Auto.prototype, "basePrice", {
+        get: function () {
+            return this._basePrice;
+        },
+        set: function (value) {
+            if (value <= 0)
+                throw 'price must be >= 0';
+            this._basePrice = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Auto.prototype, "engine", {
+        get: function () {
+            return this._engine;
+        },
+        set: function (value) {
+            if (value == undefined)
+                throw 'Please supply an engine.';
+            this._engine = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Auto.prototype.calculateTotal = function () {
+        var taxRate = .08;
+        return this.basePrice + (taxRate * this.basePrice);
+    };
+    Auto.prototype.addAccessories = function () {
+        var accessories = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            accessories[_i] = arguments[_i];
+        }
+        this.accessoryList = '';
+        for (var i = 0; i < accessories.length; i++) {
+            var ac = accessories[i];
+            this.accessoryList += ac.accessoryNumber + ' ' + ac.title + '<br />';
+        }
+    };
+    Auto.prototype.getAccessoryList = function () {
+        return this.accessoryList;
+    };
+    return Auto;
+}());
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(basePrice, engine, make, model, bedLength, fourByFour) {
+        var _this = _super.call(this, basePrice, engine, make, model) || this;
+        _this.bedLength = bedLength;
+        _this.fourByFour = fourByFour;
+        return _this;
+    }
+    return Truck;
+}(Auto));
+window.onload = function () {
+    var truck = new Truck(40000, new Engine(300, 'V8'), 'Chevy', 'Silverado', 'Long Bed', true);
+    truck.addAccessories(new Accessory(1234, 'Sunroof'), new Accessory(4321, 'Towing package'));
+    truck.engine.start(function (status, engineType) {
+        console.log(engineType + ' was started, ' + 'with status: ' + status);
+    });
+};
