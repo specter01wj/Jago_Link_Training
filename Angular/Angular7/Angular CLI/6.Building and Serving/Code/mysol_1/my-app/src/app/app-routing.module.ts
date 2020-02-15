ef942a483f3021e00ac22e15b8ea456c9798cb7e
specter01wj <specter01wj@gmail.com>
2020-02-15 00:28:06 -0500

    02/14/2020 v1

--- Angular/Angular7/Angular CLI/6.Building and Serving/Code/mysol_1/my-app/src/app/app-routing.module.ts
+++ Angular/Angular7/Angular CLI/6.Building and Serving/Code/mysol_1/my-app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'customer', component: CustomerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/Angular7/Angular CLI/6.Building and Serving/Code/mysol_1/my-app/src/app/app.module.ts
+++ Angular/Angular7/Angular CLI/6.Building and Serving/Code/mysol_1/my-app/src/app/app.module.ts
@@ -4,13 +4,50 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavComponent } from './nav/nav.component';
+
+import { AdminModule } from './admin/admin.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
