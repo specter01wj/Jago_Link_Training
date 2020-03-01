181e82da407b7dc6b10d672c4c203903886cac78
specter01wj <specter01wj@gmail.com>
2020-03-01 12:07:02 -0500

    02/29/2020 v1

--- Angular/Angular7/Angular Advanced Course/Essentials/6.Reactive Forms/Code/mysol_1/reactive-form/src/app/app.module.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/6.Reactive Forms/Code/mysol_1/reactive-form/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
--- Angular/Angular7/Angular Advanced Course/Essentials/6.Reactive Forms/Code/mysol_1/reactive-form/src/app/signup-form/signup-form.component.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/6.Reactive Forms/Code/mysol_1/reactive-form/src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { UsernameValidators } from './username.validators';
+
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
@@ -9,13 +11,48 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignupFormComponent implements OnInit {
 
 	form = new FormGroup({
-		username: new FormControl('', Validators.required),
-		password: new FormControl('', Validators.required)
+		// username: new FormControl('', Validators.required),
+    /*username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
+      // UsernameValidators.shouldBeUnique
+    ], [
+      UsernameValidators.shouldBeUnique
+    ]),
+		password: new FormControl('', Validators.required)*/
+
+    account: new FormGroup({
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace
+        ], [
+          UsernameValidators.shouldBeUnique
+        ]),
+        password: new FormControl('', Validators.required)
+    }) 
 	});
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
+
+  get username() {
+    // return this.form.get('username');
+    return this.form.get('account.username');
+  }
+
+  get password() {
+    // return this.form.get('password');
+    return this.form.get('account.password');
   }
 
 }
