7182fb07e3d53da56c479094a31c0ea8214f124b
specter01wj <specter01wj@gmail.com>
2020-03-05 19:42:10 -0500

    03/04/2020 v1

--- Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/app.module.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/app.module.ts
@@ -71,6 +71,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
+
         
         // {path: 'followers/:id', component: GithubProfileComponent},
         {path: 'followers/:id/:username', component: GithubProfileComponent},
