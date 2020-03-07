14b764d8137eaed95dcf48d27e5cabc72f4dd4ab
specter01wj <specter01wj@gmail.com>
2020-03-06 23:18:23 -0500

    03/06/2020 v1

--- Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-followers/github-followers.component.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,10 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'github-followers',
@@ -9,7 +14,9 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+  	private route: ActivatedRoute,
+  	private service: GithubFollowersService) { }
 
   ngOnInit() {
     this.service.getAll()
