0abbf0e5dbd928f0d42ee8da88e5667426e12dd3
specter01wj <specter01wj@gmail.com>
2020-03-06 23:54:18 -0500

    03/06/2020 v1

--- Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-profile/github-profile.component.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
@@ -11,15 +16,37 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-  	this.route.paramMap
+  	/*this.route.paramMap
   		.subscribe(params => {
   			// console.log(params);
   			let id = +params.get('id');
   			console.log(id);
-  		});
+  		});*/
   		// console.log("Github profile, oninit!");
   		/*let id = this.route.snapshot.paramMap.get('id');
   		console.log(id);*/
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    /*.subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+
+      // this.service.getAll({ id: id, page: page });
+      this.service.getAll()
+          .subscribe(followers => this.followers = followers);
+    });*/
+    .switchMap(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+      // return this.service.getAll();
+    })
+    .subscribe(params => {
+        // console.log(params);
+        let id = +params.get('id');
+        console.log(id);
+      });
   }
 
   submit() {
