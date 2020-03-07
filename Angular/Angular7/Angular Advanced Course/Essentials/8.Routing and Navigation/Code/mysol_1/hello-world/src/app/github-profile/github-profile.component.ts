71e50e25d1140542a7b29b8b665f87864fc02604
specter01wj <specter01wj@gmail.com>
2020-03-06 22:57:24 -0500

    03/06/2020 v1

--- Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-profile/github-profile.component.ts
+++ Angular/Angular7/Angular Advanced Course/Essentials/8.Routing and Navigation/Code/mysol_1/hello-world/src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+  	this.route.paramMap
+  		.subscribe(params => {
+  			// console.log(params);
+  			let id = +params.get('id');
+  			console.log(id);
+  		});
+  		// console.log("Github profile, oninit!");
+  		/*let id = this.route.snapshot.paramMap.get('id');
+  		console.log(id);*/
   }
 
 }
