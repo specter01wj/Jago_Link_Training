8f7c560f6964f55997d77fbab79de7a138dc1f92
specter01wj <specter01wj@gmail.com>
2020-03-16 17:33:26 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/user.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/user.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  save(user: firebase.User) {
+  	this.db.object('/users/' + user.uid).update({
+  		name: user.displayName,
+  		email: user.email
+  	});
+  }
 
 }
