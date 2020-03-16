a148f2bd17213789b11c5edfe64200bc70be9fd8
specter01wj <specter01wj@gmail.com>
2020-03-16 17:39:21 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth'; 
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './../models/app-user';
+import { UserService } from './user.service';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
 import * as firebase from 'firebase'; 
@@ -11,6 +13,7 @@ export class AuthService {
   user$: Observable<firebase.User>;
 
   constructor(
+  	private userService: UserService,
   	private afAuth: AngularFireAuth,
   	private route: ActivatedRoute) { 
   	this.user$ = afAuth.authState;
@@ -24,6 +27,13 @@ export class AuthService {
 
   logout() {
   	this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        return this.userService.get(user.uid)
+      });
   }
 
 }
