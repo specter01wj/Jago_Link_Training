21241280b2e77f88aa8f2cbc5e7bdbb86fbc9552
specter01wj <specter01wj@gmail.com>
2020-03-16 17:22:35 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth-guard.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth-guard.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(private auth: AuthService,
+  	private router: Router) { }
+
+  canActivate(route, state: RouterStateSnapshot) {
+  	return this.auth.user$.map(user => {
+  		if(user) return true;
+
+  		this.router.navigate(['/login']);
+  		return false;
+  	});
+  }
 
 }
--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth'; 
+
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  login() { 
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+  	this.afAuth.auth.signOut();
+  }
 
 }
