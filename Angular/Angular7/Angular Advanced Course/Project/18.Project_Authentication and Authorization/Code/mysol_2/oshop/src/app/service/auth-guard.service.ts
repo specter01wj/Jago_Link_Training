a107e83aa0648dd94ae50398809cbfa5209928f2
specter01wj <specter01wj@gmail.com>
2020-03-16 17:23:17 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth-guard.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth-guard.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 
+import { Router, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
 @Injectable()
 export class AuthGuardService {
 
