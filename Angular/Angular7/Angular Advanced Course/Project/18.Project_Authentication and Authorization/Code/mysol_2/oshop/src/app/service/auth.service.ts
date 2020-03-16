9b8edc787869972669d50d41aa0b71a2ceea0f9a
specter01wj <specter01wj@gmail.com>
2020-03-16 17:30:50 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/auth.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth'; 
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+import * as firebase from 'firebase'; 
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(
+  	private afAuth: AngularFireAuth,
+  	private route: ActivatedRoute) { 
+  	this.user$ = afAuth.authState;
+  }
 
   login() { 
   	let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
