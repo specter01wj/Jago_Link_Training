20d9e7585cdd4f099d6e30608d2f731510fb2a17
specter01wj <specter01wj@gmail.com>
2020-03-16 17:38:05 -0400

    03/11/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/admin-auth-guard.service.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/mysol_2/oshop/src/app/service/admin-auth-guard.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Router, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class AdminAuthGuardService {
+export class AdminAuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+  	return this.auth.user$
+  	.switchMap(user => {
+  		return this.userService.get(user.uid)
+  	})
+  	.map(appUser => appUser.isAdmin);
+  }
 
 }
