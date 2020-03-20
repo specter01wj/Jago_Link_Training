33124b883638338a5909fd7f1a2b8b3be9da952b
specter01wj <specter01wj@gmail.com>
2020-03-19 22:34:18 -0600

    03/19/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/done/oshop/src/app/login/login.component.ts
+++ Angular/Angular7/Angular Advanced Course/Project/18.Project_Authentication and Authorization/Code/done/oshop/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth'; 
+// import { AngularFireAuth } from 'angularfire2/auth'; 
 import { AuthService } from '../service/auth.service'; 
 // import * as firebase from 'firebase'; 
 
