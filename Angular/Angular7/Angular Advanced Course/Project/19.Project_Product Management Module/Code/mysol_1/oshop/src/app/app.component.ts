ee7615ac3eb503e9c381348c65acc30177190d6c
specter01wj <specter01wj@gmail.com>
2020-03-20 14:32:30 -0600

    03/20/2020 v1

---
+++ Angular/Angular7/Angular Advanced Course/Project/19.Project_Product Management Module/Code/mysol_1/oshop/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { UserService } from './user.service';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(private userService: UserService, private auth: AuthService, router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        userService.save(user);
+
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
+}
