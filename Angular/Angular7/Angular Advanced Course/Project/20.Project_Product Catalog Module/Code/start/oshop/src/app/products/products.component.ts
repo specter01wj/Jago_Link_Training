f9e8f3830e622d6c572962f2ccbc054e143fd7e1
specter01wj <specter01wj@gmail.com>
2020-03-28 00:41:55 -0600

    03/18/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/start/oshop/src/app/products/products.component.ts
+++ Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/start/oshop/src/app/products/products.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProductService } from './../product.service';
-import { Product } from './../models/product';
-import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -11,29 +7,7 @@ import 'rxjs/add/operator/switchMap';
 })
 export class ProductsComponent implements OnInit {
 
-	// products$;
-	products: Product[] = [];
-	filteredProducts: Product[] = [];
-
-	categoryBtn: string;
-
-  constructor(
-  	route: ActivatedRoute,
-  	productService: ProductService) { 
-
-  	// this.products$ = productService.getAll();
-  	productService.getAll().switchMap(products => {
-  		this.products = products;
-  		return route.queryParamMap;
-  	}).subscribe(params => {
-  		this.categoryBtn = params.get('category');
-
-  		this.filteredProducts = (this.categoryBtn) ? 
-  			this.products.filter(p => p.category === this.categoryBtn) :
-  			this.products;
-  	});;
-  	
-  }
+  constructor() { }
 
   ngOnInit() {
   }
