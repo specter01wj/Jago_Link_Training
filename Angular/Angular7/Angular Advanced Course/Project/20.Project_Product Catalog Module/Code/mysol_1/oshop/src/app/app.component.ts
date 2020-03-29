ce8ad37dc4efeb87f466fd9a736d516f9d375934
specter01wj <specter01wj@gmail.com>
2020-03-28 23:52:20 -0600

    03/18/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/mysol_1/oshop/src/app/app.component.ts
+++ Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/mysol_1/oshop/src/app/app.component.ts
@@ -16,16 +16,6 @@ export class AppComponent {
       userService.save(user);
 
       let returnUrl = localStorage.getItem('returnUrl');
-
-
-
-
-
-
-
-
-
-      
       if (!returnUrl) return; 
 
       localStorage.removeItem('returnUrl');
