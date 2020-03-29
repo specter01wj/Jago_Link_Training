d4374147c97bd57710805e218b71f722a786554b
specter01wj <specter01wj@gmail.com>
2020-03-28 18:06:18 -0600

    03/18/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/start/oshop/src/environments/environment.prod.ts
+++ Angular/Angular7/Angular Advanced Course/Project/20.Project_Product Catalog Module/Code/start/oshop/src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+  	apiKey: "AIzaSyCFp5dgqpdF9wvm9z8eqdP8cJbIXl6OZrM",
+    authDomain: "oshop-a837b.firebaseapp.com",
+    databaseURL: "https://oshop-a837b.firebaseio.com",
+    projectId: "oshop-a837b",
+    storageBucket: "oshop-a837b.appspot.com",
+    messagingSenderId: "291668278",
+    appId: "1:291668278:web:3c4b54ddce146f50b93397",
+    measurementId: "G-7E2YW93PK8"
   }
 };
