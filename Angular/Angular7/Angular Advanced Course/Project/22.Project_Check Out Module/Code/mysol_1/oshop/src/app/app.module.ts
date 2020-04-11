c067315e0294b9ba71507396bca9d70887b431f1
specter01wj <specter01wj@gmail.com>
2020-04-11 09:31:37 -0600

    04/03/2020 v1

--- Angular/Angular7/Angular Advanced Course/Project/22.Project_Check Out Module/Code/mysol_1/oshop/src/app/app.module.ts
+++ Angular/Angular7/Angular Advanced Course/Project/22.Project_Check Out Module/Code/mysol_1/oshop/src/app/app.module.ts
@@ -34,6 +34,8 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingFormComponent } from './shopping-form/shopping-form.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +53,9 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     ProductFormComponent,
     ProductFilterComponent,
     ProductCardComponent,
-    ProductQuantityComponent
+    ProductQuantityComponent,
+    ShoppingCartSummaryComponent,
+    ShoppingFormComponent
   ],
   imports: [
     BrowserModule,
