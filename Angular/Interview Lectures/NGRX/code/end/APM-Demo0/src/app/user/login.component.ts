d2c4901844d32fa311e9ca7fbcec324ff62e98bf
specter01wj <specter01wj@gmail.com>
2020-07-18 15:55:56 -0400

    06/04/2020 v1

---
+++ Angular/Interview Lectures/NGRX/code/end/APM-Demo0/src/app/user/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+import { Store, select } from '@ngrx/store';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  pageTitle = 'Log In';
+  errorMessage: string;
+
+  maskUserName: boolean;
+
+  constructor(private store: Store<any>,
+              private authService: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.store.pipe(select('users')).subscribe(
+      users => {
+        if(users) {
+          this.maskUserName = users.maskUserName;
+        }
+      }
+    );
+  }
+
+  cancel(): void {
+    this.router.navigate(['welcome']);
+  }
+
+  checkChanged(value: boolean): void {
+    // this.maskUserName = value;
+    this.store.dispatch({
+      type: "MASK_USER_NAME",
+      payload: value
+    });
+  }
+
+  login(loginForm: NgForm): void {
+    if (loginForm && loginForm.valid) {
+      const userName = loginForm.form.value.userName;
+      const password = loginForm.form.value.password;
+      this.authService.login(userName, password);
+
+      if (this.authService.redirectUrl) {
+        this.router.navigateByUrl(this.authService.redirectUrl);
+      } else {
+        this.router.navigate(['/products']);
+      }
+    } else {
+      this.errorMessage = 'Please enter a user name and password.';
+    }
+  }
+}
