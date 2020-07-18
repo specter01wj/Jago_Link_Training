a33980c4003b13773f624268720cf723abebdf0b
specter01wj <specter01wj@gmail.com>
2020-07-18 16:02:10 -0400

    06/04/2020 v1

---
+++ Angular/Interview Lectures/NGRX/code/done/APM-Demo0/src/app/products/state/product.reducer.ts
@@ -0,0 +1,18 @@
+
+
+export function reducer(state, action) {
+
+  switch(action.type) {
+    
+    case 'TOGGLE_PRODUCT_CODE':
+      return {
+        ...state,
+        showProductCode: action.payload
+      }
+
+    default:
+      return state;
+
+  }
+
+}
