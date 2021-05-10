f80b8eb4b260cc975dff22af183b890bc82717d7
Jin Wang <specter01wj@gmail.com>
2021-05-09 23:23:11 -0400

    05/07/2021 v1

---
+++ Angular/Angular7/NGRX/code/mysol_1/APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { Store, select } from '@ngrx/store';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle = 'Products';
+  errorMessage: string;
+
+  displayCode: boolean;
+
+  products: Product[];
+
+  // Used to highlight the selected product in the list
+  selectedProduct: Product | null;
+  sub: Subscription;
+
+  constructor(private store: Store<any>,
+    private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.sub = this.productService.selectedProductChanges$.subscribe(
+      selectedProduct => this.selectedProduct = selectedProduct
+    );
+
+    this.productService.getProducts().subscribe({
+      next: (products: Product[]) => this.products = products,
+      error: (err: any) => this.errorMessage = err.error
+    });
+
+    this.store.pipe(select('products')).subscribe(
+      products => {
+        if(products) {
+          this.displayCode = products.showProductCode;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  checkChanged(value: boolean): void {
+    // this.displayCode = value;
+    this.store.dispatch({
+      type: 'TOGGLE_PRODUCT_CODE',
+      payload: value
+    });
+  }
+
+  newProduct(): void {
+    this.productService.changeSelectedProduct(this.productService.newProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.productService.changeSelectedProduct(product);
+  }
+
+}
