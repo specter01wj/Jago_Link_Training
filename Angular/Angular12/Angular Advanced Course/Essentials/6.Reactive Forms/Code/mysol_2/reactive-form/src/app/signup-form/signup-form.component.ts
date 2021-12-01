c2de6cee7c774bf63c0092fe00809bd11c9afc8e
Jin Wang <specter01wj@gmail.com>
2021-12-01 00:59:02 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/Angular Advanced Course/Essentials/6.Reactive Forms/Code/mysol_2/reactive-form/src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { UsernameValidators } from './username.validators';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.less']
+})
+export class SignupFormComponent implements OnInit {
+
+	form = new FormGroup({
+		// username: new FormControl('', Validators.required),
+    /*username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
+      // UsernameValidators.shouldBeUnique
+    ], [
+      UsernameValidators.shouldBeUnique
+    ]),
+		password: new FormControl('', Validators.required)*/
+
+    account: new FormGroup({
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace
+        ], [
+          UsernameValidators.shouldBeUnique
+        ]),
+        password: new FormControl('', Validators.required)
+    }) 
+	});
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
+
+  get username() {
+    // return this.form.get('username');
+    return this.form.get('account.username');
+  }
+
+  get password() {
+    // return this.form.get('password');
+    return this.form.get('account.password');
+  }
+
+}
