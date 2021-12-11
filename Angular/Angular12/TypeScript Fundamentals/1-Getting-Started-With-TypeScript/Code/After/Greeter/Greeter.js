5b814c5214da3a36722d184b325f73655ea88e25
Jin Wang <specter01wj@gmail.com>
2021-12-11 02:13:24 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/TypeScript Fundamentals/1-Getting-Started-With-TypeScript/Code/After/Greeter/Greeter.js
@@ -0,0 +1,9 @@
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
