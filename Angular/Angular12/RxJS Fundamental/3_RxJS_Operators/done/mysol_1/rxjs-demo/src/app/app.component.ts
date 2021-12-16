ed31136976df8951f49aa24a6d3a55edbd24854a
Jin Wang <specter01wj@gmail.com>
2021-12-16 18:42:58 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/RxJS Fundamental/3_RxJS_Operators/done/mysol_1/rxjs-demo/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'rxjs-demo';
+
+  ngOnInit() {
+    of(2,4,6,8).subscribe(console.log);
+
+    from([20,15,10,5])
+      .pipe(
+        tap(item => console.log(`emitted item .... ${item}`)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if(item === 0) {
+            throw new Error('zeo detected');
+          }
+          return item;
+        }),
+        take(3)
+      ).subscribe(
+        item => console.log(`resulting item .. ${item}`),
+        err => console.error(`error occurred ${err}`),
+        () => console.log('complete')
+      );
+  }
+
+
+}
