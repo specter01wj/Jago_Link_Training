fbbd4bdf01324441314368f25e86af68a539800c
Jin Wang <specter01wj@gmail.com>
2021-12-18 13:23:03 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/Angular Advanced Course/Essentials/4.Directives/Code/mysol_1/hello-world/src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { FavChangedEventArgs } from './favorite/favorite.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'hello-world';
+
+  courses = [1 ,2];
+
+  courses_2 = [
+    {id: 1, name: "course 1"},
+    {id: 2, name: "course 2"},
+    {id: 3, name: "course 3"}
+  ];
+
+  /* courses_3 = [
+    {id: 101, name: "course 101"},
+    {id: 102, name: "course 102"},
+    {id: 103, name: "course 103"}
+  ]; */
+  courses_3 = [];
+
+  canSave = true;
+
+  viewMode = "map";
+
+  post = {
+  	title: "IBM",
+  	isFav: false
+  }
+
+  task = {
+    title: "Mission 1",
+    assignee: {
+      name: "John Smith"
+    }
+  }
+
+  loadCourses() {
+    this.courses_3 = [
+      {id: 10, name: "course 10"},
+        {id: 11, name: "course 11"},
+        {id: 12, name: "course 12"}
+    ]
+  }
+
+  trackCourse(index: any, course: any) {
+    return course ? course.id : undefined;
+  }
+
+  onAdd() {
+    this.courses_2.push( {id: 4, name: "course 4"} );
+  }
+
+  onRemove(course: any) {
+    let index = this.courses_2.indexOf(course);
+    this.courses_2.splice(index, 1);
+  }
+
+  /* onFavChanged(isFav: any) {
+  	console.log("Fav changed! " + isFav);
+  } */
+
+  onFavChanged(eventArgs: FavChangedEventArgs) {
+  	console.log("Fav changed! " + eventArgs.newVal);
+  }
+
+
+}
---
+++ Angular/Angular12/Angular Advanced Course/Essentials/4.Directives/Code/mysol_1/hello-world/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { SummaryPipePipe } from './pipes/summary-pipe.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { PanelComponent } from './panel/panel.component';
+import { InputFormatDirective } from './common/input-format.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    SummaryPipePipe,
+    FavoriteComponent,
+    PanelComponent,
+    InputFormatDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
