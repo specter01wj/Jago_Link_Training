0b11b13bc57a76810d7efab54f1d11aa18c0a473
Jin Wang <specter01wj@gmail.com>
2021-12-31 16:51:33 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/RxJS Fundamental/10_High-order_Mapping_Operators/done/mysol_2_demo2/src/app/suppliers/supplier.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { throwError, Observable, of } from 'rxjs';
+import { concatMap, map, tap } from 'rxjs/operators';
+import { Supplier } from './supplier';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+  suppliersUrl = 'api/suppliers';
+
+  suppliersWithMap$ = of(1, 5, 8)
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`)
+      )
+    );
+
+  suppliersWithConcatMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('concatMap source Observable', id)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+  constructor(private http: HttpClient) {
+    /* this.suppliersWithMap$
+      .subscribe(o => o.subscribe(
+        item => console.log('map result', item)
+      )); */
+    this.suppliersWithConcatMap$.subscribe(item => console.log('concatMap result', item));
+
+  }
+
+  private handleError(err: any): Observable<never> {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
+
+}
---
+++ Angular/Angular12/RxJS Fundamental/10_High-order_Mapping_Operators/done/mysol_2_demo4/src/app/products/product-list.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+
+import { BehaviorSubject, combineLatest, EMPTY, Observable, of, Subject, Subscription } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+
+import { Product } from './product';
+import { ProductService } from './product.service';
+import { ProductCategoryService } from '../product-categories/product-category.service';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+  selectedCategoryId = 1;
+
+  // private categorySelectedSubject = new Subject<number>();
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
+  products$ = combineLatest([
+    this.productService.productsWithAdd$,
+    // this.categorySelectedAction$.pipe(startWith(0))
+    this.categorySelectedAction$
+  ])
+    .pipe(
+      map(([products, selectedCategoryId]) =>
+        products.filter(product =>
+          selectedCategoryId ? product.categoryId === selectedCategoryId : true
+        )),
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );;
+
+  sub: Subscription;
+
+  /* productsSimpleFilter$ = this.productService.productsWithCategory$
+    .pipe(
+      map(products =>
+        products.filter(product =>
+          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+        )
+      )
+    ); */
+
+  categories$ = this.productCategoryService.productCategories$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  constructor(private productService: ProductService,
+              private productCategoryService: ProductCategoryService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    // this.sub.unsubscribe();
+  }
+
+  onAdd(): void {
+    // console.log('Not yet implemented');
+    this.productService.addProduct();
+  }
+
+  onSelected(categoryId: string): void {
+    // this.selectedCategoryId = +categoryId;
+    this.categorySelectedSubject.next(+categoryId);
+  }
+}
