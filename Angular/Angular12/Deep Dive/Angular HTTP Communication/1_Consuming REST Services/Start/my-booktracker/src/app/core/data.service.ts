e4675890ee462e4cd59b4850ca9e7fad4864c24e
Jin Wang <specter01wj@gmail.com>
2022-01-09 19:36:33 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/Deep Dive/Angular HTTP Communication/1_Consuming REST Services/Start/my-booktracker/src/app/core/data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { allBooks, allReaders } from 'app/data';
+import { Reader } from "app/models/reader";
+import { Book } from "app/models/book";
+import { BookTrackerError } from 'app/models/bookTrackerError';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  mostPopularBook: Book = allBooks[0];
+
+  setMostPopularBook(popularBook: Book): void {
+    this.mostPopularBook = popularBook;
+  }
+
+  getAllReaders(): Reader[] {
+    return allReaders;
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(reader => reader.readerID === id);
+  }
+
+  getAllBooks(): Book[] {
+    return allBooks;
+  }
+
+  getBookById(id: number): Book {
+    return allBooks.find(book => book.bookID === id);
+  }  
+}
