be52302bc38c408b9e76ae930068da97fbcea000
Jin Wang <specter01wj@gmail.com>
2022-01-11 03:10:14 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/Deep Dive/Angular HTTP Communication/1_Consuming REST Services/done/mysol_3_demo3/my-booktracker/src/app/core/data.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { allBooks, allReaders } from 'app/data';
+import { Reader } from "app/models/reader";
+import { Book } from "app/models/book";
+import { BookTrackerError } from 'app/models/bookTrackerError';
+import { OldBook } from 'app/models/oldBook';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  mostPopularBook: Book = allBooks[0];
+
+  setMostPopularBook(popularBook: Book): void {
+    this.mostPopularBook = popularBook;
+  }
+
+  getAllReaders(): Reader[] {
+    return allReaders;
+  }
+
+  getReaderById(id: number): Reader {
+    return allReaders.find(reader => reader.readerID === id);
+  }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>('/api/books');
+  }
+
+  getBookById(id: number): Observable<Book> {
+    let getHeaders: HttpHeaders = new HttpHeaders({
+      'Accept': 'application/json',
+      'Authorization': 'my-token'
+    });
+
+    return this.http.get<Book>(`/api/books/${id}`, {
+      headers: getHeaders
+    });
+  }
+
+  getOldBookById(id: number): Observable<OldBook> {
+    return this.http.get<Book>(`/api/books/${id}`)
+      .pipe(
+        map(b => <OldBook>{
+          bookTitle: b.title,
+          year: b.publicationYear
+        }),
+        tap(classicBook => console.log(classicBook))
+      );
+  }
+
+
+}
---
+++ Angular/Angular12/Deep Dive/Angular HTTP Communication/1_Consuming REST Services/done/mysol_3_demo3/my-booktracker/src/app/edit-book/edit-book.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Book } from 'app/models/book';
+import { DataService } from 'app/core/data.service';
+import { OldBook } from 'app/models/oldBook';
+
+@Component({
+  selector: 'app-edit-book',
+  templateUrl: './edit-book.component.html',
+  styles: []
+})
+export class EditBookComponent implements OnInit {
+
+  selectedBook: Book;
+
+  constructor(private route: ActivatedRoute,
+              private dataService: DataService) { }
+
+  ngOnInit() {
+    let bookID: number = parseInt(this.route.snapshot.params['id']);
+    this.dataService.getBookById(bookID)
+      .subscribe({
+        next: (res: Book) => {
+          this.selectedBook = res;
+        },
+        error: (err: any) => { console.log(err) },
+        complete: () => { console.log('All done.') }
+      });
+
+    this.dataService.getOldBookById(bookID)
+      .subscribe(
+        (data: OldBook) => console.log(`Old book title: ${data.bookTitle}`)
+      );
+  }
+
+  setMostPopular(): void {
+    this.dataService.setMostPopularBook(this.selectedBook);
+  }
+
+  saveChanges(): void {
+    console.warn('Save changes to book not yet implemented.');
+  }
+}
