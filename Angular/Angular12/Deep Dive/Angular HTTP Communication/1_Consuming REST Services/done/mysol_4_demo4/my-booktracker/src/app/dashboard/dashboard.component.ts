f00b4d91497e9a5839a489a2b82debb4de4a21ea
Jin Wang <specter01wj@gmail.com>
2022-01-12 19:33:46 -0500

    11/22/2021 v1

---
+++ Angular/Angular12/Deep Dive/Angular HTTP Communication/1_Consuming REST Services/done/mysol_4_demo4/my-booktracker/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+
+import { Book } from "app/models/book";
+import { Reader } from "app/models/reader";
+import { DataService } from 'app/core/data.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styles: []
+})
+export class DashboardComponent implements OnInit {
+
+  allBooks: Book[];
+  allReaders: Reader[];
+  mostPopularBook: Book;
+
+  constructor(private dataService: DataService,
+              private title: Title) { }
+
+  ngOnInit() {
+    this.dataService.getAllBooks()
+      .subscribe({
+        next: (res: Book[]) => {
+          this.allBooks = res;
+        },
+        error: (err: any) => { console.log(err) },
+        complete: () => { console.log('All done.') }
+      });
+    this.allReaders = this.dataService.getAllReaders();
+    this.mostPopularBook = this.dataService.mostPopularBook;
+    this.title.setTitle(`Book Tracker`);
+  }
+
+  deleteBook(bookID: number): void {
+    this.dataService.deleteBook(bookID)
+      .subscribe(
+        (data: void) => {
+          let index: number = this.allBooks.findIndex(book => book.bookID === bookID);
+          this.allBooks.splice(index, 1);
+        },
+        (err: any) => console.log(err)
+      );
+  }
+
+  deleteReader(readerID: number): void {
+    console.warn(`Delete reader not yet implemented (readerID: ${readerID}).`);
+  }
+
+}
