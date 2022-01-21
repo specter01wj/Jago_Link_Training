896ac3bc3400803b82309a49ef47da6241aeb968
Jin Wang <specter01wj@gmail.com>
2022-01-21 02:11:46 -0500

    01/12/2022 v1

--- Angular/Angular12/Deep Dive/Angular HTTP Communication/3_Creating_Interceptors/done/mysol_2_demo2/src/app/core/add-header.interceptor.ts
+++ Angular/Angular12/Deep Dive/Angular HTTP Communication/3_Creating_Interceptors/done/mysol_2_demo2/src/app/core/add-header.interceptor.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpContextToken } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-export const CONTENT_TYPE = new HttpContextToken(() => 'application/json');
 
 @Injectable()
 export class AddHeaderInterceptor implements HttpInterceptor {
@@ -10,7 +8,7 @@ export class AddHeaderInterceptor implements HttpInterceptor {
     console.log(`AddHeaderInterceptor - ${req.url}`);
 
     let jsonReq: HttpRequest<any> = req.clone({
-      setHeaders: { 'Content-Type': req.context.get(CONTENT_TYPE) }
+      setHeaders: { 'Content-Type': 'application/json' }
     });
 
     return next.handle(jsonReq);
