8a06de8ed52b4215dbe5db48e2efe1fa82d5d371
Jin Wang <specter01wj@gmail.com>
2022-02-02 03:31:23 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Routing/5_Child Routes/done/mysol_4_demo4/my-apm/src/app/products/product-routing.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductResolver } from '../service/product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags/product-edit-tags.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductListComponent
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    resolve: { resolvedData: ProductResolver }
+  },
+  {
+    path: 'products/:id/edit',
+    component: ProductEditComponent,
+    resolve: { resolvedData: ProductResolver },
+    children: [
+      {
+        path: '',
+        redirectTo: 'info',
+        pathMatch: 'full'
+      },
+      {
+        path: 'info',
+        component: ProductEditInfoComponent
+      },
+      {
+        path: 'tags',
+        component: ProductEditTagsComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ProductRoutingModule { }
