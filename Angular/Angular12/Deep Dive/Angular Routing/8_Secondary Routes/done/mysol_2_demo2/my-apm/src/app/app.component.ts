27300881fd80e3b2bb8e2779de74657229f1d70b
Jin Wang <specter01wj@gmail.com>
2022-02-04 03:18:11 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Routing/8_Secondary Routes/done/mysol_2_demo2/my-apm/src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
+import { slideInAnimation } from './api/app.animation';
+
+import { AuthService } from './service/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [slideInAnimation]
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+  loading = true;
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  get userName(): string {
+    if (this.authService.currentUser) {
+      return this.authService.currentUser.userName;
+    }
+    return '';
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+  logOut(): void {
+    // this.router.navigate(['/welcome']);
+    this.authService.logout();
+    this.router.navigateByUrl('/welcome');
+  }
+
+}
