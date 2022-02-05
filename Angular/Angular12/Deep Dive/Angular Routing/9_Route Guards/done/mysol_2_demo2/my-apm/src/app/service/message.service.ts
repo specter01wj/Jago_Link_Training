07f83018686876c46de0d66761819ad8ea77c96c
Jin Wang <specter01wj@gmail.com>
2022-02-04 20:04:46 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Routing/9_Route Guards/done/mysol_2_demo2/my-apm/src/app/service/message.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+	private _messages: string[] = [];
+  isDisplayed = false;
+
+  constructor() { }
+
+  get messages(): string[] {
+    return this._messages;
+  }
+
+  addMessage(message: string): void {
+    const currentDate = new Date();
+    this.messages.unshift(message + ' at ' + currentDate.toLocaleString());
+  }
+
+}
--- Angular/Angular12/Deep Dive/Angular Routing/9_Route Guards/done/mysol_2_demo2/my-apm/src/app/user/auth.guard.ts
+++ Angular/Angular12/Deep Dive/Angular Routing/9_Route Guards/done/mysol_2_demo2/my-apm/src/app/user/auth.guard.ts
@@ -15,14 +15,15 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.checkLoggedIn();
+    return this.checkLoggedIn(state.url);
   }
 
-  checkLoggedIn(): boolean {
+  checkLoggedIn(url: string): boolean {
     if (this.authService.isLoggedIn) {
       return true;
     }
 
+    this.authService.redirectUrl = url;
     this.router.navigate(['/login']);
     return false;
   }
