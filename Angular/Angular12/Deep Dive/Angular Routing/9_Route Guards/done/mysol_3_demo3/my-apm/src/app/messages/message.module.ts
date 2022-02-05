0bc0acc5ffbef966fcf811444720aa7d39bfa366
Jin Wang <specter01wj@gmail.com>
2022-02-04 20:51:39 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Routing/9_Route Guards/done/mysol_3_demo3/my-apm/src/app/messages/message.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MessageRoutingModule } from './message-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MessageRoutingModule
+  ]
+})
+export class MessageModule { }
