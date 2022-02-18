99aaff9a22658e808c038b2c67eae8ca81c8a878
Jin Wang <specter01wj@gmail.com>
2022-02-18 04:18:46 -0500

    01/12/2022 v1

--- Angular/Angular12/Deep Dive/Angular Routing/10_Lazy Loading/done/mysol_3_demo3/my-apm/src/app/app-routing.module.ts
+++ Angular/Angular12/Deep Dive/Angular Routing/10_Lazy Loading/done/mysol_3_demo3/my-apm/src/app/app-routing.module.ts
@@ -5,20 +5,20 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './not-found/page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
 
-const routes: Routes = [
-  { path: 'welcome', component: WelcomeComponent },
-  {
-    path: 'products',
-    canActivate: [AuthGuard],
-    loadChildren: () =>
-      import('./products/product.module').then(m => m.ProductModule)
-  },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-], { preloadingStrategy: PreloadAllModules };
+// const routes: Routes = ;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'welcome', component: WelcomeComponent },
+    {
+      path: 'products',
+      canActivate: [AuthGuard],
+      loadChildren: () =>
+        import('./products/product.module').then(m => m.ProductModule)
+    },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+  ], { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
