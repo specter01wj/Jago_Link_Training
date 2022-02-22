b8b78e59d717f966734a1a263551dfac60aa160c
Jin Wang <specter01wj@gmail.com>
2022-02-22 01:30:02 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Component Communication/2_ViewChild and ViewChildren/Start/my-apm/src/app/products/edit/product-edit-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+
+import { ProductEditComponent } from './product-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductEditGuardService implements CanDeactivate<ProductEditComponent> {
+
+  constructor() { }
+
+  canDeactivate(component: ProductEditComponent): boolean {
+      if (component.isDirty) {
+          const productName = component.product.productName || 'New Product';
+          return confirm(`Navigate away and lose all changes to ${productName}?`);
+      }
+      return true;
+  }
+  
+}
---
+++ Angular/Angular12/Deep Dive/Angular Component Communication/2_ViewChild and ViewChildren/Start/my-apm/src/app/shared/star.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StarComponent } from './star.component';
+
+describe('StarComponent', () => {
+  let component: StarComponent;
+  let fixture: ComponentFixture<StarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
