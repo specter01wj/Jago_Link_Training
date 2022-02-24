74a70a21a4cdaf845f2773b2bc90e275cd1df12d
Jin Wang <specter01wj@gmail.com>
2022-02-24 02:46:08 -0500

    01/12/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Component Communication/2_ViewChild and ViewChildren/done/mysol_2_demo2/my-apm/src/app/products/product-list.component.ts
@@ -0,0 +1,75 @@
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+
+import { ProductService } from '../service/product.service';
+
+import { IProduct } from '../interface/product';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.less']
+})
+export class ProductListComponent implements OnInit, AfterViewInit {
+	pageTitle: string = 'Product List';
+  // listFilter: string;
+  filterName: string;
+  showImage: boolean;
+
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  errorMessage: string;
+
+  @ViewChild('filterElement') filterElementRef: ElementRef;
+  // @ViewChildren('filterElement, nameElement') inputElementRefs: QueryList<ElementRef>;
+  @ViewChildren(NgModel) inputElementRefs: QueryList<ElementRef>;
+
+  private _listFilter: string;
+  get listFilter(): string {
+      return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+      this._listFilter = value;
+      this.performFilter(this.listFilter);
+  }
+
+  filteredProducts: IProduct[];
+  products: IProduct[];
+
+  constructor(private productService: ProductService) { }
+
+  ngAfterViewInit(): void {
+    this.filterElementRef.nativeElement.focus();
+    console.log(this.inputElementRefs);
+  }
+
+  ngOnInit(): void {
+  	this.productService.getProducts().subscribe(
+        (products: IProduct[]) => {
+            this.products = products;
+            this.performFilter(this.listFilter);
+        },
+        (error: any) => this.errorMessage = <any>error
+    );
+  }
+
+  onFilterChange(filter: string): void {
+    this.listFilter = filter;
+    this.performFilter(this.listFilter);
+  }
+
+  toggleImage(): void {
+      this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy?: string): void {
+      if (filterBy) {
+          this.filteredProducts = this.products.filter((product: IProduct) =>
+              product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
+      } else {
+          this.filteredProducts = this.products;
+      }
+  }
+
+}
