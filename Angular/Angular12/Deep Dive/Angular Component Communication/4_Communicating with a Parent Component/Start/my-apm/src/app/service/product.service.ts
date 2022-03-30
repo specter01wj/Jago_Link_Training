4d5a6b821f096d5f71fc6c0a508a8d353af98d12
Jin Wang <specter01wj@gmail.com>
2022-03-30 00:38:55 -0400

    03/26/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Component Communication/4_Communicating with a Parent Component/Start/my-apm/src/app/service/product.service.ts
@@ -0,0 +1,107 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+
+import { Observable, throwError, of } from 'rxjs';
+
+import { catchError, tap } from 'rxjs/operators';
+
+import { IProduct } from '../interface/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+	private productsUrl = 'api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<IProduct[]> {
+      return this.http.get<IProduct[]>(this.productsUrl)
+                      .pipe(
+                          tap(data => console.log(JSON.stringify(data))),
+                          catchError(this.handleError)
+                      );
+  }
+
+  getProduct(id: number): Observable<IProduct> {
+      if (id === 0) {
+          return of(this.initializeProduct());
+      }
+      const url = `${this.productsUrl}/${id}`;
+      return this.http.get<IProduct>(url)
+                      .pipe(
+                          tap(data => console.log('Data: ' + JSON.stringify(data))),
+                          catchError(this.handleError)
+                      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+      // in a real world app, we may send the server to some remote logging infrastructure
+      // instead of just logging it to the console
+      let errorMessage: string;
+      if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+      } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+      }
+      console.error(err);
+      return throwError(errorMessage);
+  }
+
+  deleteProduct(id: number): Observable<IProduct> {
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      const url = `${this.productsUrl}/${id}`;
+      return this.http.delete<IProduct>(url, { headers: headers} )
+                      .pipe(
+                          tap(data => console.log('deleteProduct: ' + id)),
+                          catchError(this.handleError)
+                      );
+  }
+
+  saveProduct(product: IProduct): Observable<IProduct> {
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      if (product.id === 0) {
+          return this.createProduct(product, headers);
+      }
+      return this.updateProduct(product, headers);
+  }
+
+  private createProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
+      product.id = null;
+      return this.http.post<IProduct>(this.productsUrl, product,  { headers: headers} )
+                      .pipe(
+                          tap(data => console.log('createProduct: ' + JSON.stringify(data))),
+                          catchError(this.handleError)
+                      );
+  }
+
+  private initializeProduct(): IProduct {
+      // Return an initialized object
+      return {
+          'id': 0,
+          productName: '',
+          productCode: '',
+          category: '',
+          tags: [],
+          releaseDate: '',
+          price: 0,
+          description: '',
+          starRating: 0,
+          imageUrl: ''
+      };
+  }
+
+  private updateProduct(product: IProduct, headers: HttpHeaders): Observable<IProduct> {
+      const url = `${this.productsUrl}/${product.id}`;
+      return this.http.put<IProduct>(url, product, { headers: headers} )
+                      .pipe(
+                          tap(data => console.log('updateProduct: ' + product.id)),
+                          catchError(this.handleError)
+                      );
+  }
+
+}
