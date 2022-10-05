835aef7c63b83b8abbaa915a8bc7c92d43f1286d
Jin Wang <specter01wj@gmail.com>
2022-10-05 02:05:08 -0400

    10/03/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Material/3_Layout Component/done/mysol_1_demo1/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'demo' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular/Angular12/Deep Dive/Angular Material/3_Layout Component/done/mysol_1_demo1/my-app/src/app/demo/demo.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../shared/material.module';
+import { FormsModule } from '@angular/forms';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
+
+
+@NgModule({
+  declarations: [
+    ButtonsComponent,
+    FlexboxComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    DemoRoutingModule
+  ]
+})
+export class DemoModule { }
