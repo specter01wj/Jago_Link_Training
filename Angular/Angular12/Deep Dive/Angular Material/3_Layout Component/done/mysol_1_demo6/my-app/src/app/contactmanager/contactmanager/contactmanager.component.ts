bdeef48431f46a5574065c98cf05fd4cac7e3f62
Jin Wang <specter01wj@gmail.com>
2022-10-10 00:25:34 -0400

    10/06/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular Material/3_Layout Component/done/mysol_1_demo6/my-app/src/app/contactmanager/contactmanager/contactmanager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactmanager',
+  templateUrl: './contactmanager.component.html',
+  styleUrls: ['./contactmanager.component.scss']
+})
+export class ContactmanagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
