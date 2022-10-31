8a2f88f75327dbc63872c632c6d607b657183cf8
Jin Wang <specter01wj@gmail.com>
2022-10-31 02:37:40 -0400

    10/30/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular NgRx/3_First Look at NgRx/done/mysol_1_demo2/my-app/src/app/dist/app-routing.module.js
@@ -0,0 +1,44 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppRoutingModule = void 0;
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var shell_component_1 = require("./home/shell.component");
+var welcome_component_1 = require("./home/welcome.component");
+var page_not_found_component_1 = require("./home/page-not-found.component");
+var routes = [
+    {
+        path: '',
+        component: shell_component_1.ShellComponent,
+        children: [
+            { path: 'welcome', component: welcome_component_1.WelcomeComponent },
+            {
+                path: 'products',
+                // canActivate: [AuthGuard],
+                loadChildren: function () {
+                    return Promise.resolve().then(function () { return require('./products/product.module'); }).then(function (m) { return m.ProductModule; });
+                }
+            },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+        ]
+    },
+    { path: '**', component: page_not_found_component_1.PageNotFoundComponent }
+];
+var AppRoutingModule = /** @class */ (function () {
+    function AppRoutingModule() {
+    }
+    AppRoutingModule = __decorate([
+        core_1.NgModule({
+            imports: [router_1.RouterModule.forRoot(routes)],
+            exports: [router_1.RouterModule]
+        })
+    ], AppRoutingModule);
+    return AppRoutingModule;
+}());
+exports.AppRoutingModule = AppRoutingModule;
---
+++ Angular/Angular12/Deep Dive/Angular NgRx/3_First Look at NgRx/done/mysol_1_demo2/my-app/src/app/products/product-list/dist/product-list.component.js
@@ -0,0 +1,47 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.ProductListComponent = void 0;
+var core_1 = require("@angular/core");
+var ProductListComponent = /** @class */ (function () {
+    function ProductListComponent(store, productService) {
+        this.store = store;
+        this.productService = productService;
+        this.pageTitle = 'Products';
+    }
+    ProductListComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.sub = this.productService.selectedProductChanges$.subscribe(function (currentProduct) { return _this.selectedProduct = currentProduct; });
+        this.productService.getProducts().subscribe({
+            next: function (products) { return _this.products = products; },
+            error: function (err) { return _this.errorMessage = err; }
+        });
+    };
+    ProductListComponent.prototype.ngOnDestroy = function () {
+        this.sub.unsubscribe();
+    };
+    ProductListComponent.prototype.checkChanged = function () {
+        // this.displayCode = !this.displayCode;
+        this.store.dispatch({ type: '[Product] Toggle Product Code' });
+    };
+    ProductListComponent.prototype.newProduct = function () {
+        this.productService.changeSelectedProduct(this.productService.newProduct());
+    };
+    ProductListComponent.prototype.productSelected = function (product) {
+        this.productService.changeSelectedProduct(product);
+    };
+    ProductListComponent = __decorate([
+        core_1.Component({
+            selector: 'pm-product-list',
+            templateUrl: './product-list.component.html',
+            styleUrls: ['./product-list.component.css']
+        })
+    ], ProductListComponent);
+    return ProductListComponent;
+}());
+exports.ProductListComponent = ProductListComponent;
