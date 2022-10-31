de468b1ea2bab10dadb609252d005c1fdeef8ec8
Jin Wang <specter01wj@gmail.com>
2022-10-31 01:15:25 -0400

    10/30/2022 v1

---
+++ Angular/Angular12/Deep Dive/Angular NgRx/3_First Look at NgRx/done/mysol_1_demo1/my-app/src/app/products/state/product.reducer.ts
@@ -0,0 +1,13 @@
+import { createReducer, on, createAction } from '@ngrx/store';
+
+export const productReducer = createReducer(
+  { showProductCode: true },
+  on(createAction('[Product] Toggle Product Code'), state => {
+    return {
+      ...state,
+      showProductCode: !state.showProductCode
+      // showProductCode: state.showProductCode = false,
+    };
+  })
+);
+
