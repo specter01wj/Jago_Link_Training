c9870f331f482135b69fd2f1d47c16bba9117f9d
Jin Wang <specter01wj@gmail.com>
2023-12-28 18:54:00 -0500

    v1

---
+++ Angular/Angular12/Deep Dive/Angular Material/4_Display Real Data/done/mysol_1_demo1/my-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedAngularMaterialModule } from './shared/angular-material.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedAngularMaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/Angular12/Deep Dive/Angular Material/4_Display Real Data/done/mysol_1_demo1/my-app/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -0,0 +1,39 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+  public isScreenSmall: boolean = true;
+
+  users: Observable<User[]>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private userService: UserService,
+  ) { }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)` ])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
+
+    this.users = this.userService.users;
+    this.userService.loadAll();
+
+    this.users.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+}
