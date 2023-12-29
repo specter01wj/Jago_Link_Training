1ef7b624ae82d10d46a7a54fa990c81ed3821321
Jin Wang <specter01wj@gmail.com>
2023-12-29 02:20:36 -0500

    v9

---
+++ Angular/Angular12/Deep Dive/Angular Material/5_Using Data Tables/done/mysol_1_demo4/my-app/src/app/contactmanager/components/notes/notes.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Note } from '../../models/note';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrl: './notes.component.scss'
+})
+export class NotesComponent implements OnInit, AfterViewInit {
+  @Input() notes: Note[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['position', 'title', 'date' ];
+  dataSource: MatTableDataSource<Note>;
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
